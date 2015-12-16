ca758c3a69856e89a3e060c9907d7e4111ea0e18
Adam Hair <iamadamhair@gmail.com>
2015-12-16 12:42:22 -0600

    Updated color schemes, now can change default colors in settings page

--- tips/TipViewController.swift
+++ tips/TipViewController.swift
@@ -21,6 +21,12 @@ class TipViewController: UIViewController {
     @IBOutlet var backgroundView: UIView!
     @IBOutlet weak var plusTipLabel: UILabel!
     @IBOutlet weak var grandTotalLabel: UILabel!
+    @IBOutlet weak var dollarLabel: UILabel!
+    @IBOutlet weak var threeWaysLabel: UILabel!
+    @IBOutlet weak var billLabel: UILabel!
+    @IBOutlet weak var twoWaysLabel: UILabel!
+    @IBOutlet weak var fourWaysLabel: UILabel!
+    @IBOutlet weak var fiveWaysLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -58,13 +64,13 @@ class TipViewController: UIViewController {
     }
 
     func setAppColor() {
-        var colorIndex = 1
+        var colorIndex = 0
         
         if defaultSet() {
             // Update colors
             colorIndex = NSUserDefaults.standardUserDefaults().integerForKey("defaultColorIndex")
         }
-        
+        /*
         var redBackgrounds: [Int] = [255,167,30]
         var greenBackgrounds = [132,255,181]
         var blueBackgrounds = [121,144,255]
@@ -72,16 +78,81 @@ class TipViewController: UIViewController {
         var redForegrounds = [249,0,23]
         var greenForegrounds = [44,147,26]
         var blueForegrounds = [42,27,255]
+        */
         
-        // Background colors
-       // backgroundView.backgroundColor = UIColor(red: redBackgrounds[colorIndex-1]/255.0, green: CGFloat(greenBackgrounds[colorIndex-1]/255), blue: CGFloat(blueBackgrounds[colorIndex-1]/255), alpha: 1.0)
-        //totalLabel.textColor = UIColor(red: CGFloat(redBackgrounds[colorIndex-1]/255), green: CGFloat(greenBackgrounds[colorIndex-1]/255), blue: CGFloat(blueBackgrounds[colorIndex-1]/255), alpha: 1.0)
-        //tipLabel.textColor = UIColor(red: CGFloat(redBackgrounds[colorIndex-1]/255), green: CGFloat(greenBackgrounds[colorIndex-1]/255), blue: CGFloat(blueBackgrounds[colorIndex-1]/255), alpha: 1.0)
-        //plusTipLabel.textColor = UIColor(red: CGFloat(redBackgrounds[colorIndex-1]/255), green: CGFloat(greenBackgrounds[colorIndex-1]/255), blue: CGFloat(blueBackgrounds[colorIndex-1]/255), alpha: 1.0)
-        //grandTotalLabel.textColor = UIColor(red: CGFloat(redBackgrounds[colorIndex-1]/255), green: CGFloat(greenBackgrounds[colorIndex-1]/255), blue: CGFloat(blueBackgrounds[colorIndex-1]/255), alpha: 1.0)
-        
-        // Foreground Colors
-        //blackBarView.backgroundColor = UIColor(red: CGFloat(redForegrounds[colorIndex-1]/255), green: CGFloat(greenForegrounds[colorIndex-1]/255), blue: CGFloat(blueForegrounds[colorIndex-1]/255), alpha: 1.0)
+        if(colorIndex == 2) {
+            // Blue
+            // Background colors
+            backgroundView.backgroundColor = UIColor(red: 0x1E/255.0, green: 0xB5/255.0, blue: 0xFF/255.0, alpha: 1.0)
+            totalLabel.textColor = UIColor(red: 0x1E/255.0, green: 0xB5/255.0, blue: 0xFF/255.0, alpha: 1.0)
+            tipLabel.textColor = UIColor(red: 0x1E/255.0, green: 0xB5/255.0, blue: 0xFF/255.0, alpha: 1.0)
+            plusTipLabel.textColor = UIColor(red: 0x1E/255.0, green: 0xB5/255.0, blue: 0xFF/255.0, alpha: 1.0)
+            grandTotalLabel.textColor = UIColor(red: 0x1E/255.0, green: 0xB5/255.0, blue: 0xFF/255.0, alpha: 1.0)
+            
+            // Foreground Colors
+            dollarLabel.textColor = UIColor(red: 0x17/255.0, green: 0x1A/255.0, blue: 0xFF/255.0, alpha: 1.0)
+            blackBarView.backgroundColor = UIColor(red: 0x17/255.0, green: 0x1A/255.0, blue: 0xFF/255.0, alpha: 1.0)
+            billField.textColor = UIColor(red: 0x17/255.0, green: 0x1A/255.0, blue: 0xFF/255.0, alpha: 1.0)
+            billLabel.textColor = UIColor(red: 0x17/255.0, green: 0x1A/255.0, blue: 0xFF/255.0, alpha: 1.0)
+            tipControl.tintColor = UIColor(red: 0x17/255.0, green: 0x1A/255.0, blue: 0xFF/255.0, alpha: 1.0)
+            twoWayField.textColor = UIColor(red: 0x17/255.0, green: 0x1A/255.0, blue: 0xFF/255.0, alpha: 1.0)
+            twoWaysLabel.textColor = UIColor(red: 0x17/255.0, green: 0x1A/255.0, blue: 0xFF/255.0, alpha: 1.0)
+            threeWayField.textColor = UIColor(red: 0x17/255.0, green: 0x1A/255.0, blue: 0xFF/255.0, alpha: 1.0)
+            threeWaysLabel.textColor = UIColor(red: 0x17/255.0, green: 0x1A/255.0, blue: 0xFF/255.0, alpha: 1.0)
+            fourWayField.textColor = UIColor(red: 0x17/255.0, green: 0x1A/255.0, blue: 0xFF/255.0, alpha: 1.0)
+            fourWaysLabel.textColor = UIColor(red: 0x17/255.0, green: 0x1A/255.0, blue: 0xFF/255.0, alpha: 1.0)
+            fiveWayField.textColor = UIColor(red: 0x17/255.0, green: 0x1A/255.0, blue: 0xFF/255.0, alpha: 1.0)
+            fiveWaysLabel.textColor = UIColor(red: 0x17/255.0, green: 0x1A/255.0, blue: 0xFF/255.0, alpha: 1.0)
+        }
+        else if(colorIndex == 1) {
+            // Green
+            // Background colors
+            backgroundView.backgroundColor = UIColor(red: 0xA7/255.0, green: 0xFF/255.0, blue: 0x90/255.0, alpha: 1.0)
+            totalLabel.textColor = UIColor(red: 0xA7/255.0, green: 0xFF/255.0, blue: 0x90/255.0, alpha: 1.0)
+            tipLabel.textColor = UIColor(red: 0xA7/255.0, green: 0xFF/255.0, blue: 0x90/255.0, alpha: 1.0)
+            plusTipLabel.textColor = UIColor(red: 0xA7/255.0, green: 0xFF/255.0, blue: 0x90/255.0, alpha: 1.0)
+            grandTotalLabel.textColor = UIColor(red: 0xA7/255.0, green: 0xFF/255.0, blue: 0x90/255.0, alpha: 1.0)
+            
+            // Foreground Colors
+            dollarLabel.textColor = UIColor(red: 0x00/255.0, green: 0x93/255.0, blue: 0x1B/255.0, alpha: 1.0)
+            blackBarView.backgroundColor = UIColor(red: 0x00/255.0, green: 0x93/255.0, blue: 0x1B/255.0, alpha: 1.0)
+            billField.textColor = UIColor(red: 0x00/255.0, green: 0x93/255.0, blue: 0x1B/255.0, alpha: 1.0)
+            billLabel.textColor = UIColor(red: 0x00/255.0, green: 0x93/255.0, blue: 0x1B/255.0, alpha: 1.0)
+            tipControl.tintColor = UIColor(red: 0x00/255.0, green: 0x93/255.0, blue: 0x1B/255.0, alpha: 1.0)
+            twoWayField.textColor = UIColor(red: 0x00/255.0, green: 0x93/255.0, blue: 0x1B/255.0, alpha: 1.0)
+            twoWaysLabel.textColor = UIColor(red: 0x00/255.0, green: 0x93/255.0, blue: 0x1B/255.0, alpha: 1.0)
+            threeWayField.textColor = UIColor(red: 0x00/255.0, green: 0x93/255.0, blue: 0x1B/255.0, alpha: 1.0)
+            threeWaysLabel.textColor = UIColor(red: 0x00/255.0, green: 0x93/255.0, blue: 0x1B/255.0, alpha: 1.0)
+            fourWayField.textColor = UIColor(red: 0x00/255.0, green: 0x93/255.0, blue: 0x1B/255.0, alpha: 1.0)
+            fourWaysLabel.textColor = UIColor(red: 0x00/255.0, green: 0x93/255.0, blue: 0x1B/255.0, alpha: 1.0)
+            fiveWayField.textColor = UIColor(red: 0x00/255.0, green: 0x93/255.0, blue: 0x1B/255.0, alpha: 1.0)
+            fiveWaysLabel.textColor = UIColor(red: 0x00/255.0, green: 0x93/255.0, blue: 0x1B/255.0, alpha: 1.0)
+        }
+        else {
+            // Red
+            // Background colors
+            backgroundView.backgroundColor = UIColor(red: 0xFF/255.0, green: 0x84/255.0, blue: 0x79/255.0, alpha: 1.0)
+            totalLabel.textColor = UIColor(red: 0xFF/255.0, green: 0x84/255.0, blue: 0x79/255.0, alpha: 1.0)
+            tipLabel.textColor = UIColor(red: 0xFF/255.0, green: 0x84/255.0, blue: 0x79/255.0, alpha: 1.0)
+            plusTipLabel.textColor = UIColor(red: 0xFF/255.0, green: 0x84/255.0, blue: 0x79/255.0, alpha: 1.0)
+            grandTotalLabel.textColor = UIColor(red: 0xFF/255.0, green: 0x84/255.0, blue: 0x79/255.0, alpha: 1.0)
+            
+            // Foreground Colors
+            dollarLabel.textColor = UIColor(red: 0xF9/255.0, green: 0x2C/255.0, blue: 0x2A/255.0, alpha: 1.0)
+            blackBarView.backgroundColor = UIColor(red: 0xF9/255.0, green: 0x2C/255.0, blue: 0x2A/255.0, alpha: 1.0)
+            billField.textColor = UIColor(red: 0xF9/255.0, green: 0x2C/255.0, blue: 0x2A/255.0, alpha: 1.0)
+            billLabel.textColor = UIColor(red: 0xF9/255.0, green: 0x2C/255.0, blue: 0x2A/255.0, alpha: 1.0)
+            tipControl.tintColor = UIColor(red: 0xF9/255.0, green: 0x2C/255.0, blue: 0x2A/255.0, alpha: 1.0)
+            twoWayField.textColor = UIColor(red: 0xF9/255.0, green: 0x2C/255.0, blue: 0x2A/255.0, alpha: 1.0)
+            twoWaysLabel.textColor = UIColor(red: 0xF9/255.0, green: 0x2C/255.0, blue: 0x2A/255.0, alpha: 1.0)
+            threeWayField.textColor = UIColor(red: 0xF9/255.0, green: 0x2C/255.0, blue: 0x2A/255.0, alpha: 1.0)
+            threeWaysLabel.textColor = UIColor(red: 0xF9/255.0, green: 0x2C/255.0, blue: 0x2A/255.0, alpha: 1.0)
+            fourWayField.textColor = UIColor(red: 0xF9/255.0, green: 0x2C/255.0, blue: 0x2A/255.0, alpha: 1.0)
+            fourWaysLabel.textColor = UIColor(red: 0xF9/255.0, green: 0x2C/255.0, blue: 0x2A/255.0, alpha: 1.0)
+            fiveWayField.textColor = UIColor(red: 0xF9/255.0, green: 0x2C/255.0, blue: 0x2A/255.0, alpha: 1.0)
+            fiveWaysLabel.textColor = UIColor(red: 0xF9/255.0, green: 0x2C/255.0, blue: 0x2A/255.0, alpha: 1.0)
+        }
+
     }
     
     override func didReceiveMemoryWarning() {
